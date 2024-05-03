b472b3a0a7dda7bdaf28b8d901788174ac14d7a5
Álvaro Cuesta <1827495+alvaro-cuesta@users.noreply.github.com>
2024-05-03 11:32:23 +0200

    Better RGB matrix config for my keymap

---
+++ keyboards/keychron/v6/iso_encoder/keymaps/alvaro-cuesta/config_indicators.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#undef CAPS_LOCK_LED_INDEX
+#define CAPS_LOCK_LED_INDEX 60 // normal caps lock key index
+
+#define NUM_LOCK_LED_INDEX 37 // normal num lock key index
+
+#define SCROLL_LOCK_LED_INDEX 14 // scroll lock key index -- positioned where it usually is in other keyboards, microphone key in V6 (Siri/Cortana)
+
+#define NKRO_LED_INDEX 85 // N key
+
+#define FORCE_TURN_OFF_INDICATORS // force turning off indicators when RGB effects are on
