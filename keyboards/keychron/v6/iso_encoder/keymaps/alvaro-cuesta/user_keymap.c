e05d8df7abe21cef44fc32ff3b351a10830086e2
Álvaro Cuesta <1827495+alvaro-cuesta@users.noreply.github.com>
2024-05-03 19:10:17 +0200

    Assign RGB effects to specific keys.

--- keyboards/keychron/v6/iso_encoder/keymaps/alvaro-cuesta/user_keymap.c
+++ keyboards/keychron/v6/iso_encoder/keymaps/alvaro-cuesta/user_keymap.c
@@ -24,6 +24,46 @@
 #include "user_keycodes.h"
 #include "user_layers.h"
 
+// Aliases to keep the keymap clean and aligned
+#define RGBM_1  RGBM_SOLID_COLOR
+#define RGBM_2  RGBM_BREATHING
+#define RGBM_3  RGBM_RIVERFLOW
+#define RGBM_4  RGBM_BAND_VAL
+#define RGBM_5  RGBM_BAND_SAT
+#define RGBM_6  RGBM_BAND_PINWHEEL_VAL
+#define RGBM_7  RGBM_BAND_PINWHEEL_SAT
+#define RGBM_8  RGBM_BAND_SPIRAL_VAL
+#define RGBM_9  RGBM_BAND_SPIRAL_SAT
+#define RGBM_0  RGBM_GRADIENT_UP_DOWN
+#define RGBM_AP RGBM_GRADIENT_LEFT_RIGHT
+
+#define RGBM_Q  RGBM_CYCLE_ALL
+#define RGBM_W  RGBM_CYCLE_LEFT_RIGHT
+#define RGBM_E  RGBM_CYCLE_UP_DOWN
+#define RGBM_R  RGBM_RAINBOW_MOVING_CHEVRON
+#define RGBM_T  RGBM_CYCLE_PINWHEEL
+#define RGBM_Y  RGBM_CYCLE_SPIRAL
+#define RGBM_U  RGBM_DUAL_BEACON
+#define RGBM_I  RGBM_RAINBOW_BEACON
+#define RGBM_O  RGBM_CYCLE_OUT_IN
+#define RGBM_P  XXXXXXX
+
+#define RGBM_A  RGBM_TYPING_HEATMAP
+#define RGBM_S  RGBM_SOLID_REACTIVE_SIMPLE
+#define RGBM_D  RGBM_SOLID_REACTIVE
+#define RGBM_F  RGBM_SOLID_REACTIVE_MULTIWIDE
+#define RGBM_G  RGBM_SOLID_REACTIVE_MULTICROSS
+#define RGBM_H  RGBM_SOLID_REACTIVE_MULTINEXUS
+#define RGBM_J  RGBM_MULTISPLASH
+#define RGBM_K  RGBM_SOLID_MULTISPLASH
+#define RGBM_L  XXXXXXX
+
+#define RGBM_Z  RGBM_FLOWER_BLOOMING
+#define RGBM_X  RGBM_RAINDROPS
+#define RGBM_C  RGBM_JELLYBEAN_RAINDROPS
+#define RGBM_V  RGBM_PIXEL_RAIN
+#define RGBM_B  XXXXXXX
+
 // clang-format off
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -37,11 +77,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD, KC_ROPTN, MO(MAC_FN1), KC_RCTL,        KC_LEFT,  KC_DOWN,  KC_RGHT,        KC_P0,              KC_PDOT,  KC_PENT),
 
     [MAC_FN1] = LAYOUT_iso_110(
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,      RGB_TOG,        _______,  _______,  RGB_TOG,        _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,        _______,  _______,  _______,        _______,  _______,  _______,  _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                     _______,  _______,  _______,        _______,  _______,  _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,                                            _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  RGBE_HU,  RGBE_SA,  RGBE_SP,               RGBE_EF,                  _______,                  _______,  _______,  _______,
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,      RGB_TOG,        _______,  _______,  KC_PWR,         _______,  _______,  _______,  MC_0,
+        _______,  RGBM_1,   RGBM_2,   RGBM_3,   RGBM_4,   RGBM_5,   RGBM_6,   RGBM_7,   RGBM_8,   RGBM_9,   RGBM_0,   RGBM_AP,  _______,     _______,        _______,  _______,  _______,        _______,  _______,  _______,  _______,
+        RGB_TOG,  RGBM_Q,   RGBM_W,   RGBM_E,   RGBM_R,   RGBM_T,   RGBM_Y,   RGBM_U,   RGBM_I,   RGBM_O,   RGBM_P,   _______,  _______,                     _______,  _______,  _______,        _______,  _______,  _______,
+        _______,  RGBM_A,   RGBM_S,   RGBM_D,   RGBM_F,   RGBM_G,   RGBM_H,   RGBM_J,   RGBM_K,   RGBM_L,   _______,  _______,  _______,     _______,                                            _______,  _______,  _______,  _______,
+        _______,  RGBM_Z,   RGBM_X,   RGBM_C,   RGBM_V,   RGBM_B,   _______,  NK_TOGG,  RGBE_EF,  RGBE_HU,  RGBE_SA,  RGBE_SP,               _______,                  _______,                  _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,     _______,        _______,  _______,  _______,        _______,            _______,  _______),
 
     [MAC_FN2] = LAYOUT_iso_110(
@@ -63,10 +103,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_FN1] = LAYOUT_iso_110(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,     RGB_TOG,        _______,  _______,  KC_PWR,         _______,  _______,  _______,  MC_0,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,        _______,  _______,  _______,        _______,  _______,  _______,  _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                     _______,  _______,  _______,        _______,  _______,  _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,                                            _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  RGBE_HU,  RGBE_SA,  RGBE_SP,               RGBE_EF,                  _______,                  _______,  _______,  _______,
+        _______,  RGBM_1,   RGBM_2,   RGBM_3,   RGBM_4,   RGBM_5,   RGBM_6,   RGBM_7,   RGBM_8,   RGBM_9,   RGBM_0,   RGBM_AP,  _______,     _______,        _______,  _______,  _______,        _______,  _______,  _______,  _______,
+        RGB_TOG,  RGBM_Q,   RGBM_W,   RGBM_E,   RGBM_R,   RGBM_T,   RGBM_Y,   RGBM_U,   RGBM_I,   RGBM_O,   RGBM_P,   _______,  _______,                     _______,  _______,  _______,        _______,  _______,  _______,
+        _______,  RGBM_A,   RGBM_S,   RGBM_D,   RGBM_F,   RGBM_G,   RGBM_H,   RGBM_J,   RGBM_K,   RGBM_L,   _______,  _______,  _______,     _______,                                            _______,  _______,  _______,  _______,
+        _______,  RGBM_Z,   RGBM_X,   RGBM_C,   RGBM_V,   RGBM_B,   _______,  NK_TOGG,  RGBE_EF,  RGBE_HU,  RGBE_SA,  RGBE_SP,               _______,                  _______,                  _______,  _______,  _______,
         _______,  KC_APP,   _______,                                _______,                                _______,  KC_APP,   _______,     _______,        _______,  _______,  _______,        _______,            _______,  _______),
 
     [WIN_FN2] = LAYOUT_iso_110(
